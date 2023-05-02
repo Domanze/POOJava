f528296ba2a8ea58caeded6598f10ce55599ee57
Marco Mendoza <menday2004@gmail.com>
2023-05-01 20:15:15 -0600

    CRUD PRODUCTOS

--- src/main/java/com/mycompany/poojava/TestConnection.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.poojava;
-
-public class TestConnection {
-    public static void main(String[] args){
-        CConnection objconnection = new CConnection();
-        objconnection.establishConnection();
-    }
-}
